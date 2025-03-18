a89c0a76ba8701b34c7c907206d2c31852536910
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-03-18 19:20:21 +0700

    Program Pengulangan While

---
+++ LoopingSwitchCase2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    srand(time(0));
+    rand()%10;
+    //Perulangan While
+    while
+    //Perulangan DoWhile
+}
