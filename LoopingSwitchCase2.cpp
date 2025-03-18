06d7d730793693d38fccd41bb831064be8871c1d
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-03-18 19:40:12 +0700

    Program Looping While

--- LoopingSwitchCase2.cpp
+++ LoopingSwitchCase2.cpp
@@ -4,9 +4,27 @@
 using namespace std;
 
 int main(){
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
     srand(time(0));
     rand()%10;
+    cout<< "Perulangan While"<<endl;
+    cout << "Nilai X awal = " << x << endl;
     //Perulangan While
-    while
-    //Perulangan DoWhile
+    while (x < 7)
+    {
+        cout<< "Nilai X = " << x << "Lari Keliling" << endl;
+        x = rand() %10;
+        perulanganWhile++;
+    }
+    cout << "Nilai X terakhir = " <<endl;
+    cout << "Jumlah Perulangan =" << perulanganWhile <<endl;
+
+    do
+    {
+    
+    } while ();
+    
 }
