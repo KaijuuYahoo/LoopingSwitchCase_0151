3f4bc14c56a7417e799d0e97e8ffe0b7b448230a
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-03-18 19:52:18 +0700

    Pogram Pengulangan Do While

--- LoopingSwitchCase2.cpp
+++ LoopingSwitchCase2.cpp
@@ -9,22 +9,32 @@ int main(){
     int perulanganDo = 0;
 
     srand(time(0));
-    rand()%10;
+    x = rand()%10;
+
+    //Perulangan While
+
     cout<< "Perulangan While"<<endl;
     cout << "Nilai X awal = " << x << endl;
-    //Perulangan While
     while (x < 7)
     {
-        cout<< "Nilai X = " << x << "Lari Keliling" << endl;
-        x = rand() %10;
+        cout<< "Nilai X = " << x << " Lari Keliling" << endl;
+        x = rand() % 10;
         perulanganWhile++;
     }
     cout << "Nilai X terakhir = " <<endl;
     cout << "Jumlah Perulangan =" << perulanganWhile <<endl;
 
+    //Perulangan Do While  
+    cout << endl;
+    cout << "Perulangan Do While" << endl;
     do
     {
+        cout<< "Nilai X = " << x << " Lari Keliling" << endl;
+        x = rand() %10;
+        perulanganDo++;
+    } while (x < 7);
     
-    } while ();
+    cout << "Nilai X terakhir = " <<endl;
+    cout << "Jumlah Perulangan =" << perulanganDo <<endl;
     
 }
